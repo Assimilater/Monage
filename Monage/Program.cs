e29a965dd11ab782e7ae9e7c915972dc1839de76
John Call <johnkc1992@gmail.com>
2014-05-18 23:58:07 -0600

    Add unadded files

---
+++ Monage/Program.cs
@@ -0,0 +1,22 @@
+﻿using Monage.GUI;
+using Monage.Migrations;
+using Monage.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monage {
+    static class Program {
+        [STAThread]
+        static void Main() {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Context, Configuration>());
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainFrame());
+        }
+    }
+}
