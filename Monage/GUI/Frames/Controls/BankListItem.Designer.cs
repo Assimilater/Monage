55e92e9ba03ebd120674b4db038565fa18f8dca9
John Call <johnkc1992@gmail.com>
2014-05-27 19:39:05 -0600

    Create BankListItem

---
+++ Monage/GUI/Frames/Controls/BankListItem.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Monage.GUI.Frames.Controls {
+    partial class BankListItem {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.refRename = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(3, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(138, 29);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Bank Name";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.Location = new System.Drawing.Point(4, 29);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(155, 24);
+            this.lblAmount.TabIndex = 1;
+            this.lblAmount.Text = "$123.45 [$121.85]";
+            // 
+            // refRename
+            // 
+            this.refRename.AutoSize = true;
+            this.refRename.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refRename.ForeColor = System.Drawing.Color.Blue;
+            this.refRename.Location = new System.Drawing.Point(4, 53);
+            this.refRename.Name = "refRename";
+            this.refRename.Size = new System.Drawing.Size(71, 20);
+            this.refRename.TabIndex = 2;
+            this.refRename.Text = "Rename";
+            this.refRename.Click += new System.EventHandler(this.refRename_Click);
+            // 
+            // BankListItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.refRename);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblName);
+            this.Name = "BankListItem";
+            this.Size = new System.Drawing.Size(264, 74);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.Label refRename;
+    }
+}
